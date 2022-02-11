818b53194902b457b09b862d2e6a88801ef564d1
Ativelox <ativelox.dev@web.de>
2022-02-11 02:32:11 +0100

    Changes to gitignore

---
+++ src/de/ativelox/feo/client/model/property/EPlayerAction.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package de.ativelox.feo.client.model.property;
+
+/**
+ * @author Julian Tischner <juliantischner97@gmail.com>
+ *
+ */
+public enum EPlayerAction {
+    ATTACK, WAIT;
+
+}
